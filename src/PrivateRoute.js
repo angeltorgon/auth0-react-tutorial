f2ba980fef9da7025f25060e1c6880e94d64685c
angel-torres <alas4224@gmail.com>
2019-07-14 23:48:49 -0500

    done making changes

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -3,8 +3,6 @@ import { Route, Redirect } from 'react-router-dom';
 
 import Auth from './Auth';
 
-
-
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const auth = new Auth();
   return (
